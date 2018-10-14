2acefff1962a5c768aced7ddde253ab41d26bb98
ifilot <ivo@zuidstijl.nl>
2018-10-14 10:40:16 +0200

    Initial commit writing man page

--- build_deb.sh
+++ build_deb.sh
@@ -9,13 +9,16 @@ VERSION_MICRO=`grep "VERSION_MICRO" ./src/CMakeLists.txt | awk '{print $2}' | gr
 # build directories
 DISTPATH=${PACKAGE_NAME}-${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}
 mkdir -pv deb/${DISTPATH}
-mkdir -pv deb/${DISTPATH}/{DEBIAN,usr/bin,usr/share/doc/edp}
+mkdir -pv deb/${DISTPATH}/{DEBIAN,usr/bin,usr/share/man/man1,usr/share/doc/${PACKAGE_NAME}}
 
 # copy files
-sed -e "2 s/$/ ${PACKAGE_NAME}-${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}/" package/copyright > deb/${DISTPATH}/usr/share/doc/edp/copyright
+sed -e "1 s/$/ ${PACKAGE_NAME}-${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO} stretch; urgency=low/" package/changelog > changelog
+gzip -9 -n -c changelog > deb/${DISTPATH}/usr/share/doc/${PACKAGE_NAME}/changelog.gz
+sed -e "2 s/$/ ${PACKAGE_NAME}-${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}/" package/copyright > deb/${DISTPATH}/usr/share/doc/${PACKAGE_NAME}/copyright
 sed -e "1 s/$/ ${PACKAGE_NAME}-${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}/" package/control > deb/${DISTPATH}/DEBIAN/control
 sed -i "9 s/$/ ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}/" deb/${DISTPATH}/DEBIAN/control
-objcopy --strip-debug --strip-unneeded ./build/edp deb/${DISTPATH}/usr/bin/edp
+objcopy --strip-debug --strip-unneeded ./build/${PACKAGE_NAME} deb/${DISTPATH}/usr/bin/${PACKAGE_NAME}
+gzip -9 -n -c package/man/${PACKAGE_NAME} > deb/${DISTPATH}/usr/share/man/man1/${PACKAGE_NAME}.1.gz
 
 # build package
 pushd deb
@@ -25,3 +28,4 @@ popd
 
 # clean-up folder
 rm -rvf deb
+rm -vf changelog
