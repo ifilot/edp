5e351ef592a9fc7fcdad42535e166ebff0f3e3f0
ifilot <ivo@ivofilot.nl>
2023-04-12 13:41:23 +0200

    Expanding paper and fixing headers in code

---
+++ paper/compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Perform Docker compilation under Windows using Git BASH 
+
+# provide the path to the location where the paper is stored; note that under
+# Windows Git Bash, two slashes need to be used instead of one
+PATH="//d//PROGRAMMING//CPP//edp//paper"
+
+docker run --rm \
+--volume $PATH:/data \
+--user $(id -u):$(id -g) \
+--env JOURNAL=joss \
+openjournals/inara
--- src/cmake/modules/FindCPPUNIT.cmake
+++ src/cmake/modules/FindCPPUNIT.cmake
@@ -1,3 +1,23 @@
+ #*************************************************************************
+ #   CMakeLists.txt  --  This file is part of edp.                        *
+ #                                                                        *
+ #   Author: Ivo Filot <i.a.w.filot@tue.nl>                               *
+ #                                                                        *
+ #   edp is free software: you can redistribute it and/or modify          *
+ #   it under the terms of the GNU General Public License as published    *
+ #   by the Free Software Foundation, either version 3 of the License,    *
+ #   or (at your option) any later version.                               *
+ #                                                                        *
+ #   edp is distributed in the hope that it will be useful,               *
+ #   but WITHOUT ANY WARRANTY; without even the implied warranty          *
+ #   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              *
+ #   See the GNU General Public License for more details.                 *
+ #                                                                        *
+ #   You should have received a copy of the GNU General Public License    *
+ #   along with this program.  If not, see http://www.gnu.org/licenses/.  *
+ #                                                                        *
+ #*************************************************************************/
+
 #
 # Find the CppUnit includes and library
 #
